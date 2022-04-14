9083124933939e0315ef80e2bafdf255b8b63731
taz71191 <murtazav2@gmail.com>
2022-04-13 21:30:49 -0400

    change port to run server

--- worker.py
+++ worker.py
@@ -1,4 +1,7 @@
 from mojek_service.app import app
+import os
+# https://stackoverflow.com/questions/17260338/deploying-flask-with-heroku
 
 if __name__ == "__main__":
-  app.run()
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
