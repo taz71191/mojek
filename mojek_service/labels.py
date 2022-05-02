2341f499f401f3cf9b124569b6a24cc31c768dce
taz71191 <murtazav2@gmail.com>
2022-05-01 22:16:20 -0400

    update labels

--- mojek_service/labels.py
+++ mojek_service/labels.py
@@ -1,6 +1,6 @@
 labels = ['Credit Card Payment', 'Equity', 'Crypto', 'Savings',
        'Household Expenses', 'Pet Expense', 'General Expenses', 'Fuel', 'Retail',
-       'Online Shopping', 'Streaming', 'Flight', 'Hotel', 'Local travel',
+       'Online Shopping', 'Streaming', 'Hotel', 'Local travel',
        'Restaurants', 'Work Expenses', 'Online News', 
        'ATM Withdrawal', 'Clothing', 'Air Travel', 'Alcohol & Bars', 'Amusement',
        'Bills & Utilities', 'Coffee Shops', 'Groceries', 'Health & Fitness',
